8acbf25fadd26a34397392a1b6d3efb50aa94a3c
Slumhunden <kroghbastin@gmail.com>
2023-05-11 10:17:50 +0200

    login dialog

--- js/app.js
+++ js/app.js
@@ -4,4 +4,22 @@ window.addEventListener("load", initApp);
 
 function initApp() {
 	console.log("Hej");
+	document.querySelector("#loginDiv").addEventListener("click", showLogin)
+}
+function showLogin(){
+	document.querySelector("#loginDialog").showModal()
+	document.querySelector("#login-form").addEventListener("submit", validateLogin)
+	document.querySelector("#login-form").reset();
+}
+function validateLogin(event){
+	event.preventDefault();
+	const username = document.querySelector("#loginUsername").value;
+	const password = document.querySelector("#loginPassword").value;
+
+	if(username ===	user.username && password === user.password){
+
+	}else{
+		alert("login mislykkedes")
+	}
+
 }
