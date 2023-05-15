ba1f036b566ec2ab58ac30a14c4518f0eaa75e5f
Sofie Larsen <Sofieamalierl@hotmail.com>
2023-05-15 10:45:53 +0200

    create user form - ikke færdig

--- js/admin.js
+++ js/admin.js
@@ -6,6 +6,8 @@ let users
 function initApp(){
     console.log("admin view loaded")
     fetchUsers()
+
+    document.querySelector("#createUser").addEventListener("click", showCreateUserDialog);
 }
 async function fetchUsers(){
 users = await getUsers()
@@ -28,4 +30,29 @@ function adminViewUsers(usersList){
     for(const user of usersList ){
         adminViewUser(user)
     }
+}
+
+function showCreateUserDialog() {
+	document.querySelector("#dialog-create-user").showModal();
+	document.querySelector("#form-create-new-user").addEventListener("submit", createUserClicked);
+}
+
+async function createUserClicked(event) {
+	event.preventDefault();
+	const form = this;
+	const name = form.name.value;
+	const age = form.age.value;
+	const role = form.role_select.value;
+	const mail = form.mail.value;
+	const team = form.team_select.value;
+	const operatingSystem = form.discipliner.value;
+	const pineapple = form.pineapple.value;
+	const response = await createUser(name, age, role, mail, team, discipliner, pineapple);
+	if (response.ok) {
+		console.log("User added to Firebase!");
+		form.reset();
+		document.querySelector("#dialog-create-user").close();
+		updateUsersGrid();
+		showPrompt("User added to Firebase!");
+	}
 }
