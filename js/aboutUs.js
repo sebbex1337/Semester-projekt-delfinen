891933c3f9b33c80743fd4eb247a5d9fa0b0b5a0
sebbex1337 <sefort@hotmail.dk>
2023-05-22 08:54:46 +0200

    removed redundant code

--- js/aboutUs.js
+++ js/aboutUs.js
@@ -1,9 +1,7 @@
-import { validateLogin, showLogin } from "./index.js";
+"use strict";
 
 window.addEventListener("load", initApp);
 
 function initApp() {
-	document.querySelector("#loginDiv").addEventListener("click", showLogin);
-	document.querySelector("#login-form").addEventListener("submit", validateLogin);
 	document.querySelector("#delfinLogo").addEventListener("click", () => (window.location.href = "index.html"));
 }
